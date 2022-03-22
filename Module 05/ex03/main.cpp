51c0d386b41a233338ca033e45818eef991a59bd
Kirill Zaychenko <haykdavis@mail.ru>
2022-03-22 14:20:55 +0300

    main.cpp

--- Module 05/ex03/main.cpp
+++ Module 05/ex03/main.cpp
@@ -3,146 +3,108 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "Intern.hpp"
 
 int main(){
-{
-	Bureaucrat bureaucrat("Bob", 1);
-	ShrubberyCreationForm form_1("target");
-	RobotomyRequestForm form_2("target");
-	PresidentialPardonForm form_3("target");
+	{
+		std::cout << std::endl << std::endl;
 
-	try
-	{
-		bureaucrat.executeForm(form_1);
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	try
-	{
-		bureaucrat.executeForm(form_2);
-	}
+		Intern intern;
+		Bureaucrat bureaucrat("Bob", 55);
+
+		Form *form = intern.makeForm("bad request", "target");
+		Form *form_1 = intern.makeForm("shrubbery request", "target");
+		Form *form_2 = intern.makeForm("robotonomy request", "target");
+		Form *form_3 = intern.makeForm("presidential request", "target");
+
+
+		std::cout
+			<< form << std::endl
+			<< *form_1  << " " << form_1 << std::endl
+			<< *form_2 << " " << form_2 << std::endl
+			<< *form_3 << " " << form_3 << std::endl;
+
+
+		std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" << std::endl;
+		try
+		{
+			bureaucrat.executeForm(*form_1);
+		}
 		catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	try
-	{
-		bureaucrat.executeForm(form_3);
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-	try
-	{
-		bureaucrat.signForm(form_1);
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	try
-	{
-		bureaucrat.signForm(form_2);
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	try
-	{
-		bureaucrat.signForm(form_3);
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-	try
-	{
-		bureaucrat.executeForm(form_1);
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	try
-	{
-		bureaucrat.executeForm(form_2);
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	try
-	{
-		bureaucrat.executeForm(form_3);
-	}
-	catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
+		{
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			bureaucrat.executeForm(*form_2);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			bureaucrat.executeForm(*form_3);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 
-	std::cout << std::endl << std::endl;
+		std::cout << std::endl;
+		try
+		{
+			bureaucrat.signForm(*form_1);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			bureaucrat.signForm(*form_2);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			bureaucrat.signForm(*form_3);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 
-}
-{
-	Bureaucrat bureaucrat("Bob", 55);
-	ShrubberyCreationForm form_1("target_1");
-	RobotomyRequestForm form_2("target_1");
-	PresidentialPardonForm form_3("target_1");
+		std::cout << std::endl;
+		try
+		{
+			bureaucrat.executeForm(*form_1);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			bureaucrat.executeForm(*form_2);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			bureaucrat.executeForm(*form_3);
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 
-	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" << std::endl;
-	try{
-		bureaucrat.executeForm(form_1);
-	} catch (std::exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	try{
-		bureaucrat.executeForm(form_2);
-	} catch (std::exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	try{
-		bureaucrat.executeForm(form_3);
-	} catch (std::exception &e){
-		std::cout << e.what() << std::endl;
-	}
-
-	std::cout << std::endl;
-	try{
-		bureaucrat.signForm(form_1);
-	} catch (std::exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	try{
-		bureaucrat.signForm(form_2);
-	} catch (std::exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	try{
-		bureaucrat.signForm(form_3);
-	} catch (std::exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-	try{
-		bureaucrat.executeForm(form_1);
-	} catch (std::exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	try{
-		bureaucrat.executeForm(form_2);
-	} catch (std::exception &e){
-		std::cout << e.what() << std::endl;
-	}
-	try{
-		bureaucrat.executeForm(form_3);
-	} catch (std::exception &e){
-		std::cout << e.what() << std::endl;
-	}
+		delete form_1;
+		delete form_2;
+		delete form_3;
 	}
 }
